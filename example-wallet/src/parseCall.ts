8dbb957fcca4ada2f66f170f8fc2d46fa803a09b
jarvandev <131938987+jarvandev@users.noreply.github.com>
2025-11-10 14:36:54 +0800

    fix: fix prepareCall function

--- example-wallet/src/parseCall.ts
+++ example-wallet/src/parseCall.ts
@@ -4,7 +4,7 @@
 import type { KeyringPair } from '@polkadot/keyring/types';
 
 import { ApiPromise } from '@polkadot/api';
-import { bnToU8a } from '@polkadot/util';
+// import { bnToU8a } from '@polkadot/util';
 
 export const prepareCall = async (
   api: ApiPromise,
@@ -16,26 +16,37 @@ export const prepareCall = async (
 
   const jwkProvider = api.createType('PrimitiveZkloginJwkProvider', 'Google');
 
+  const convertToBytes = (value: string | number) => {
+    const u256Value = api.createType('U256', value);
+    const bytesArray = Array.from(u256Value.toU8a());
+
+    return api.createType('Bytes', bytesArray);
+  };
+
   const inputs = api.createType('PrimitiveZkloginZkInputZkLoginInputs', {
     proofPoints: {
-      a: proof.proof_points.a, // u256
-      b: proof.proof_points.b,
-      c: proof.proof_points.c
+      a: api.createType('Vec<Bytes>', proof.proof_points.a.map(convertToBytes)),
+      b: api.createType(
+        'Vec<Vec<Bytes>>',
+        proof.proof_points.b.map((subArr: (string | number)[]) => subArr.map(convertToBytes))
+      ),
+      c: api.createType('Vec<Bytes>', proof.proof_points.c.map(convertToBytes))
     },
     issBase64Details: {
-      value: bnToU8a(BigInt(proof.iss_base64_details.value)),
-      indexMod4: proof.iss_base64_details.index_mod_4
+      value: api.createType('U256', BigInt(proof.iss_base64_details.value).toString()),
+      indexMod4: api.createType('u8', proof.iss_base64_details.index_mod_4)
     },
-    header: bnToU8a(BigInt(proof.header))
+    header: api.createType('U256', BigInt(proof.header).toString())
   });
 
-  const zkMaterial = api.createType('PrimitiveZkloginZkMaterial', {
-    provider: jwkProvider,
-    kid: kid,
-    inputs: inputs,
-    ephkeyExpireAt: maxEpoch
+  const zkMaterial = api.createType('PrimitiveZkloginVersionedZkMaterial', {
+    V1: {
+      provider: jwkProvider,
+      kid: kid,
+      inputs: inputs,
+      ephkey_expire_at: api.createType('u64', maxEpoch)
+    }
   });
-
   const { nonce }: any = await api.query.system.account(zkAddress);
   const uxt = call.sign(pair, {
     blockHash: api.genesisHash,
